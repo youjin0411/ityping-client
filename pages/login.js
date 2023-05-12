38cfd4a2e87a772fdf8467382a45d82d1dfa8616
hyennin <chm050202@gmail.com>
2023-05-12 22:13:56 +0900

    login ui 구현 완료

--- pages/login.js
+++ pages/login.js
@@ -1,52 +1,31 @@
 import styles from '@/styles/Login.module.css';
 import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { loginUser } from '../_actions/user_login';
 
 const Login = () => {
-    // const dispatch = useDispatch();
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+    
+    const handleIdChange = (e) => {
+        setId(e.target.value);
+    }
 
-    // const [Email, setEmail] = useState("");
-    // const [Password, setPassword] = useState("");
-
-    // const onEmailHandler = (event) => {
-    //     setEmail(event.currentTarget.value);
-    // }
-    // const onPasswordHandler = (event) => {
-    //     setPassword(event.currentTarget.value);
-    // }
-    // const onSubmitHandler = (event) => {
-    //     // 버튼만 누르면 리로드 되는것을 막아줌
-    //     event.preventDefault();
-
-    //     console.log('Email', Email);
-    //     console.log('Password', Password);
-        
-    //     let body = {
-    //         email: Email,
-    //         password: Password,
-    //     }
-
-    //     dispatch(loginUser(body));
-    // }
+    const handlePwChange = (e) => {
+        setPw(e.target.value);
+    }
 
     return (
         <div className={styles.container}>
             <h1 className={styles.welcome}>Welcome!</h1>
-            <p className={styles.text}>Login to ITyping</p>
-            {/* <form className={styles.form} onSubmit={onSubmitHandler}> */}
+            <p className={styles.text}>ITyping 로그인하기</p>
             <form className={styles.form}>
                 <label className={styles.label}>Email</label>
-                {/* <input className={styles.input_email} type='email' value={Email} onChange={onEmailHandler} placeholder='Email을 입력해주세요'/> */}
-                <input className={styles.input} type='email' placeholder='Email을 입력해주세요'/>
+                <input className={styles.input} type='email' placeholder='Email을 입력해주세요' onChange={handleIdChange}/>
                 <label className={styles.label}>Password</label>
-                {/* <input className={styles.input_pw} type='password' value={Password} onChange={onPasswordHandler} placeholder='Password를 입력해주세요'/> */}
-                <input className={styles.input} type='password' placeholder='Password를 입력해주세요'/>
-                {/* <button className={styles.login_btn} formAction=''>Login</button> */}
-                <button className={styles.login_btn}>Login</button>
+                <input className={styles.input} type='password' placeholder='Password를 입력해주세요' onChange={handlePwChange}/>
+                <button className={styles.login_btn} disabled={id.length === 0 || pw.length === 0}>Login</button>
             </form>
             <div className={styles.or}>OR</div>
-            <button className={styles.google_btn}>Sign in with Google</button>
+            <button className={styles.google_btn}><img className={styles.google_img} src='google.png'/>Sign in with Google</button>
         </div>
     )
 }   
