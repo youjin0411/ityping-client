2fad7f53e8eae42eea446d2f7b3987a43186a1e0
hyennin <chm050202@gmail.com>
2023-05-12 21:31:44 +0900

    login ui 구현

--- pages/login.js
+++ pages/login.js
@@ -1,163 +1,54 @@
+import styles from '@/styles/Login.module.css';
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import { useDispatch } from 'react-redux';
-import { loginUser } from '../_actions/user_login';
+// import { useDispatch } from 'react-redux';
+// import { loginUser } from '../_actions/user_login';
 
 const Login = () => {
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
 
-    const [Email, setEmail] = useState("");
-    const [Password, setPassword] = useState("");
+    // const [Email, setEmail] = useState("");
+    // const [Password, setPassword] = useState("");
 
-    const onEmailHandler = (event) => {
-        setEmail(event.currentTarget.value);
-    }
-    const onPasswordHandler = (event) => {
-        setPassword(event.currentTarget.value);
-    }
-    const onSubmitHandler = (event) => {
-        // 버튼만 누르면 리로드 되는것을 막아줌
-        event.preventDefault();
+    // const onEmailHandler = (event) => {
+    //     setEmail(event.currentTarget.value);
+    // }
+    // const onPasswordHandler = (event) => {
+    //     setPassword(event.currentTarget.value);
+    // }
+    // const onSubmitHandler = (event) => {
+    //     // 버튼만 누르면 리로드 되는것을 막아줌
+    //     event.preventDefault();
 
-        console.log('Email', Email);
-        console.log('Password', Password);
+    //     console.log('Email', Email);
+    //     console.log('Password', Password);
         
-        let body = {
-            email: Email,
-            password: Password,
-        }
+    //     let body = {
+    //         email: Email,
+    //         password: Password,
+    //     }
 
-        dispatch(loginUser(body));
-    }
+    //     dispatch(loginUser(body));
+    // }
 
     return (
-        <Container>
-            <Welcome>Welcome!</Welcome>
-            <Text>Login to ITyping</Text>
-            <Form onSubmit={onSubmitHandler}>
-                <Label>Email</Label>
-                <InputEmail type='email' value={Email} onChange={onEmailHandler} placeholder='Email을 입력해주세요'/>
-                <Label>Password</Label>
-                <InputPw type='password' value={Password} onChange={onPasswordHandler} placeholder='Password를 입력해주세요'/>
-                <LoginBtn formAction=''>Login</LoginBtn>
-            </Form>
-            <Or>OR</Or>
-            <Button>Sign in with Google</Button>
-        </Container>
+        <div className={styles.container}>
+            <h1 className={styles.welcome}>Welcome!</h1>
+            <p className={styles.text}>Login to ITyping</p>
+            {/* <form className={styles.form} onSubmit={onSubmitHandler}> */}
+            <form className={styles.form}>
+                <label className={styles.label}>Email</label>
+                {/* <input className={styles.input_email} type='email' value={Email} onChange={onEmailHandler} placeholder='Email을 입력해주세요'/> */}
+                <input className={styles.input_email} type='email' placeholder='Email을 입력해주세요'/>
+                <label className={styles.label}>Password</label>
+                {/* <input className={styles.input_pw} type='password' value={Password} onChange={onPasswordHandler} placeholder='Password를 입력해주세요'/> */}
+                <input className={styles.input_pw} type='password' placeholder='Password를 입력해주세요'/>
+                {/* <button className={styles.login_btn} formAction=''>Login</button> */}
+                <button className={styles.login_btn}>Login</button>
+            </form>
+            <div className={styles.or}>OR</div>
+            <button className={styles.google_btn}>Sign in with Google</button>
+        </div>
     )
 }   
 
-const Container = styled.div`
-    margin: 146px auto;
-    width: 445px;
-    height: 592px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    border: 1px solid #667085;
-    border-radius: 10px;
-`;
-
-const Welcome = styled.h1`
-    color: #1C3796;
-    font-size: 25px;
-    margin: 0;
-    margin-top: 40px;
-`;
-
-const Text = styled.p`
-    font-size: 16px;
-    margin: 17px 0;
-`;
-
-const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-`;
-
-const Label = styled.label`
-    color: #38405D;  
-
-    &:nth-child(1) {
-        margin-top: 38px;
-    }
-
-    &:nth-child(3) {
-        margin-top: 16px;
-    }
-`;
-
-const InputEmail = styled.input`
-    width: 236px;
-    height: 42px;
-    margin-top: 8px;
-    border: 1px solid #667085;
-    border-radius: 8px;
-    padding-left: 10px;
-    outline: 0;
-`;
-
-const InputPw = styled.input`
-    width: 236px;
-    height: 42px;
-    margin-top: 8px;
-    border: 1px solid #667085;
-    border-radius: 8px;
-    padding-left: 10px;
-    outline: 0;
-`;
-
-const LoginBtn = styled.button`
-    width: 250px;
-    height: 38px;
-    margin-top: 18px;
-    color: white;
-    border: 1px solid #667085;
-    background-color: #1C3796;
-    border-radius: 8px;
-    font-size: 16px;
-`;
-
-const Or = styled.div`
-    display: flex;
-    align-items: center;
-    color: #38405D;
-    font-size: 12px;
-    margin-top: 34px;
-    margin-bottom: 22px;
-
-    &::before {
-        content: "";
-        width: 99px;
-        margin: 0 16px;
-        background: #38405D;
-        height: 1px;
-        font-size: 0px;
-        line-height: 0px;
-        margin: 0px 16px;
-    }
-
-    &::after {
-        content: "";
-        width: 99px;
-        margin: 0 16px;
-        background: #38405D;
-        height: 1px;
-        font-size: 0px;
-        line-height: 0px;
-        margin: 0px 16px;
-    }
-`;
-
-const Button = styled.button`
-    width: 250px;
-    height: 42px;
-    background-color: #ffffff;
-    color: #757575;
-    border: 1px solid #667085;
-    border-radius: 8px;
-    font-weight: bold;
-    font-size: 16px;
-`;
 export default Login;
