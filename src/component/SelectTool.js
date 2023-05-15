77348c2c65ef2073c7d27ee032ec237121f61d31
Hyemin Choi <90095211+hyennin@users.noreply.github.com>
2023-05-15 13:18:33 +0900

    💄 add: SelectTool ui 구현

---
+++ src/component/SelectTool.js
@@ -0,0 +1,38 @@
+import styles from "@/styles/SelectTool.module.css";
+import React from "react";
+
+const SelectTool = (props) => {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.box} style={{backgroundColor: props.background_color}}>For {props.job_en}</div>
+        <div className={styles.content_container}>
+          <h4>{props.job_ko}를 위한 툴</h4>
+          <div className={styles.tools_container}>
+            {props.tools.map((tool) => {
+              return (
+                <div className={styles.tool_container} key={tool.name}>
+                  <img className={styles.tool_img} src={tool.img} />
+                  <div className={styles.tool_contents}>
+                    <h3>{tool.name}</h3>
+                    <p className={styles.tool_info}>{tool.info}</p>
+                  </div>
+                  <img className={styles.move_img} src='move.png'/>
+                </div>
+              );
+            })}
+            <div className={styles.tool_container}>
+              <img className={styles.locked_tool_img} src='locked_tool.png'/>
+              <div className={styles.tool_contents}>
+                <h3 className={styles.locked_text}>오픈 예정</h3>
+              </div>
+              <img className={styles.lockced_move_img} src='locked_move.png'/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SelectTool;
