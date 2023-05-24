286d9eff31b23b4335b7bd24aa8aeb4da0e9c669
hyennin <chm050202@gmail.com>
2023-05-25 08:19:15 +0900

    ✨ add: shortcut, emmat 데이터 추가

---
+++ public/shortcut.js
@@ -0,0 +1,87 @@
+const shortcuts = [
+    {
+      shortcut: "Ctrl + P",
+      description: "파일 열기",
+    },
+    {
+      shortcut: "Ctrl + W",
+      description: "현재 열려있는 파일 닫기",
+    },
+    {
+      shortcut: "Ctrl + Shift + W",
+      description: "열려있는 모든 파일 닫기",
+    },
+    {
+      shortcut: "Ctrl + S",
+      description: "파일 저장",
+    },
+    {
+      shortcut: "Ctrl + Tab",
+      description: "탭 이동",
+    },
+    {
+      shortcut: "Ctrl + Q",
+      description: "이전에 사용한 파일 열기",
+    },
+    {
+      shortcut: "Ctrl + 좌측 화살표 / Ctrl + 우측 화살표",
+      description: "단어 단위로 커서 이동",
+    },
+    {
+      shortcut: "Ctrl + L",
+      description: "라인 전체 선택",
+    },
+    {
+      shortcut: "Ctrl + Shift + K",
+      description: "라인 삭제",
+    },
+    {
+      shortcut: "Ctrl + D",
+      description: "줄 복사",
+    },
+    {
+      shortcut: "Ctrl + Shift + L",
+      description: "전체 줄 복사",
+    },
+    {
+      shortcut: "Ctrl + /",
+      description: "문장 단위 주석",
+    },
+    {
+      shortcut: "Ctrl + Shift + P",
+      description: "Command Palette를 오픈",
+    },
+    {
+      shortcut: "Ctrl + P",
+      description: "전체 디렉토리에서 '파일명' 검색",
+    },
+    {
+      shortcut: "Ctrl + Shift + Tab",
+      description: "최근에 열었던 파일들간 이동",
+    },
+    {
+      shortcut: "Ctrl + Tab",
+      description: "열려있는 모든 파일 사이에서 이동",
+    },
+    {
+      shortcut: "Ctrl + \\",
+      description: "현재 파일을 두 개의 세로 분할 패널로 나눠줌",
+    },
+    {
+      shortcut: "Ctrl + 1-9",
+      description: "열려있는 파일을 숫자키로 이동",
+    },
+    {
+        shortcut: "Ctrl + F2",
+        description: "모든 항목 변경"
+    },
+    {
+        shortcut: "Alt + 방향키 위/아래",
+        description: "코드 행 변경"
+    },
+    {
+        shortcut: "Alt + Ctrl + 방향키 위/아래",
+        description: "커서를 여러 개 만듦"
+    }
+];
+  
