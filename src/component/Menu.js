055493baa2e576f67f0750fdd96dce3dd0077fdc
hyennin <chm050202@gmail.com>
2023-05-31 08:01:46 +0900

    ♻ fix: 경로 변경

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -3,8 +3,8 @@ import styles from '@/styles/Menu.module.css';
 const Menu = (props) => {
     return (
         <div className={styles.container}>
-            <div className={styles.menu_btn}><a>연습</a></div>
-            <div className={styles.menu_btn}><a href={props.isShortcut ? '/practice/shortcut' : '/practice/emmat'}>실습</a></div>
+            <div className={styles.menu_btn}><a href={props.isShortcut ? '/practice/shortcut' : '/practice/emmat'}>연습</a></div>
+            <div className={styles.menu_btn}><a>실습</a></div>
             <div className={styles.menu_btn}><a>게임</a></div>
         </div>
     );
